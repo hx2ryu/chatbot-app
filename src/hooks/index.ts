b7a9c20c76016a4195470f44fa4259847c4ae4a6
hx2ryu <hx2ryu@gmail.com>
2022-01-24 18:00:25 +0900

    chore: add safe-area-hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+import useGetSafeAreaStyle from './useGetSafeAreaStyles';
+
+export { useGetSafeAreaStyle };
---
+++ src/hooks/useGetSafeAreaStyles.ts
@@ -0,0 +1,16 @@
+import { StyleProp, ViewStyle } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const useGetSafeAreaStyle = () => {
+  const { top, bottom } = useSafeAreaInsets();
+  return {
+    topSafeAreaStyle: {
+      paddingTop: top > 16 ? top : 16,
+    } as StyleProp<ViewStyle>,
+    bottomSafeAreaStyle: {
+      paddingBottom: bottom > 16 ? bottom : 16,
+    } as StyleProp<ViewStyle>,
+  };
+};
+
+export default useGetSafeAreaStyle;
