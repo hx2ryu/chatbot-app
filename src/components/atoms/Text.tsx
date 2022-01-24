113c4c6f09e374a60776b3a08dacbd232fe77765
hx2ryu <hx2ryu@gmail.com>
2022-01-24 16:04:44 +0900

    feat: implement text

---
+++ src/components/atoms/Text.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FONTS, FontType } from '@/theme';
+import { Text as TextBase, TextProps } from 'react-native';
+
+interface Props extends TextProps {
+  type: FontType;
+}
+const Text: React.FC<Props> = ({ children, style, type }) => (
+  <TextBase style={[FONTS[type], style]}>{children}</TextBase>
+);
+
+export default Text;
